c32d7b7a2ec1cfe84f9fdaf73b77a0b5db79de78
tanaka akira <tanaka.web.create@gmail.com>
2019-12-14 14:53:52 +0900

    fix: import firebase

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,4 +1,4 @@
-import firebase from 'firebase';
+import * as firebase from 'firebase';
 import { firebaseConfig } from './config';
 
 export const firebaseApp = firebase.initializeApp(firebaseConfig);
