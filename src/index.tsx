681964b233832e68868e5ffaba1bc4931860e8f0
tanaka akira <tanaka.web.create@gmail.com>
2019-12-14 15:26:36 +0900

    add: styled-compoonents

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
 import * as React from 'react';
 import {render} from 'react-dom';
 import AppChat from './containers/AppChat';
+import {createGlobalStyle} from 'styled-components'
+import reset from 'styled-reset'
+createGlobalStyle`
+  ${reset}
+`
 
 render(<AppChat />, document.getElementById('app'));
