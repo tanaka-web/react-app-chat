cf824ee0cdae26b596b85a8c086930dbc71103a0
tanaka akira <tanaka.web.create@gmail.com>
2019-12-16 03:42:20 +0900

    firebase config 情報を git から削除

--- src/firebase/config.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyArPC2Ph52p_7A6SG-9R__6ljLwo9n87mo',
-  authDomain: 'react-app-chat-439c2.firebaseapp.com',
-  databaseURL: 'https://react-app-chat-439c2.firebaseio.com',
-  projectId: 'react-app-chat-439c2',
-  storageBucket: 'react-app-chat-439c2.appspot.com',
-  messagingSenderId: '183243883072',
-  appId: '1:183243883072:web:c6a6f023b8ea84e66495a5',
-};
